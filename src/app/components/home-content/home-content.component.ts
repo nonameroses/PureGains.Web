4d41457cfc4635135529fbe5bd16f9ffb61dd721
Adomas Garalis <U2078658@unimail.hud.ac.uk>
2024-02-04 14:08:29 +0000

    ssss

--- src/app/components/home-content/home-content.component.ts
+++ src/app/components/home-content/home-content.component.ts
@@ -1,14 +1,183 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faLink } from '@fortawesome/free-solid-svg-icons';
+import { Equipment } from 'src/app/shared/models/equipment-interface';
+import { MuscleGroup } from 'src/app/shared/models/muscle-group.interface';
+import { PageSelectionModel } from 'src/app/shared/models/pageSelectionModel-interface';
+import { WorkoutExercise } from 'src/app/shared/models/workout-exercise.interface';
+import { Workout } from 'src/app/shared/models/workout.interface';
+import { EquipmentService } from 'src/app/shared/services/equipment.service';
+import { MuscleGroupService } from 'src/app/shared/services/muscle-group.service';
 
 @Component({
   selector: 'app-home-content',
   templateUrl: './home-content.component.html',
   styleUrls: ['./home-content.component.css'],
   standalone: true,
-  imports: [FontAwesomeModule]
+  imports: [FontAwesomeModule, CommonModule]
 })
 export class HomeContentComponent {
+  pages: PageSelectionModel[] = [
+    {
+      pageId: 'equipment',
+    },
+    {
+      pageId: 'muscleGroupSelection',
+    },
+    {
+      pageId: 'workoutPage',
+    },
+  ];
   faLink = faLink;
+  active: boolean = false;
+
+  selectedEquipment: Equipment[] = [];
+  selectedMuscleGroups: MuscleGroup[] = [];
+
+  equipment: Equipment[] = [];
+  muscleGroups: MuscleGroup[] = [];
+
+  workoutExercises: WorkoutExercise[] = [
+    {
+      id: 1,
+      exerciseId: 1,
+      workoutId: 1,
+      name: 'Pull-Up',
+      isSelected: false,
+    },
+    {
+      id: 1,
+      exerciseId: 4,
+      workoutId: 1,
+      name: 'Chin-Up',
+      isSelected: false,
+    },
+    {
+      id: 1,
+      exerciseId: 3,
+      workoutId: 1,
+      name: 'Swing',
+      isSelected: false,
+    },
+    {
+      id: 1,
+      exerciseId: 2,
+      workoutId: 1,
+      name: 'Dada',
+      isSelected: false,
+    },
+  ];
+  activeWorkout: Workout = {
+    id:1,
+    exercises: this.workoutExercises
+  }
+
+  constructor(
+    private equipmentService: EquipmentService,
+    private muscleGroupService: MuscleGroupService,
+    
+  ) {}
+
+  ngOnInit() {
+    this.equipmentService
+      .getEquipment()
+      .subscribe((response) =>
+        (this.equipment = response).map((x) => (x.isSelected = false))
+      );
+
+    this.muscleGroupService
+      .getMuscleGroups()
+      .subscribe((response) =>
+        (this.muscleGroups = response).map((x) => (x.isSelected = false))
+      );
+  }
+  getSelectedEquipment(): Equipment[] {
+    this.selectedEquipment = this.equipment.filter((e) => e.isSelected);
+    return this.selectedEquipment;
+  }
+  getSelectedMuscleGroup(event: any): MuscleGroup[] {
+    var currentSelection = event?.target.id;
+
+    this.selectedMuscleGroups = this.muscleGroups.filter((e) => e.isSelected);
+
+    if (
+      currentSelection === 'Full-Body' &&
+      this.selectedMuscleGroups.length > 1
+    ) {
+      this.selectedMuscleGroups
+        .filter((x) => x.name !== 'Full-Body')
+        .map((x) => (x.isSelected = false));
+
+      this.selectedMuscleGroups
+        .filter((x) => x.name === 'Full-Body')
+        .map((x) => (x.isSelected = true));
+
+      this.selectedMuscleGroups = this.selectedMuscleGroups.filter(
+        (x) => x.name === 'Full-Body'
+      );
+    } else {
+      this.selectedMuscleGroups
+        .filter((x) => x.name === 'Full-Body')
+        .map((x) => (x.isSelected = false));
+      this.selectedMuscleGroups = this.selectedMuscleGroups.filter(
+        (x) => x.name !== 'Full-Body'
+      );
+    }
+    console.log(this.selectedMuscleGroups);
+    return this.selectedMuscleGroups;
+  }
+
+  progressSelection() {
+    if (this.selectedEquipment.length > 0) {
+      this.active = true;
+    }
+  }
+
+  public waitAndGoDown(id: string) {
+    if (this.selectedEquipment.length > 0) {
+      this.waitForElement(id);
+      this.active = true;
+    }
+  }
+
+  public waitAndGoUp(id: string) {
+    this.waitForElement(id);
+  }
+
+  waitForElement(selector: any) {
+    let element = document.getElementById(selector);
+    if (element) {
+      scroll(selector);
+      return;
+    }
+    let observer = new MutationObserver((mutations) => {
+      mutations.forEach(function (mutation) {
+        let nodes = Array.from(mutation.addedNodes);
+        for (var node of nodes) {
+          if (node.contains(document.getElementById(selector))) {
+            scroll(selector);
+            observer.disconnect();
+            return;
+          }
+        }
+      });
+    });
+    observer.observe(document.documentElement, {
+      childList: true,
+      subtree: true,
+    });
+  }
+}
+
+function scroll(selector: string) {
+  const element = document.getElementById(selector);
+
+  if (element !== null) {
+    element.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest',
+    });
+  }
 }
