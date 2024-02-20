1c604c0aecedc5763f1296f104e453d905de7464
Adomas Garalis <U2078658@unimail.hud.ac.uk>
2024-02-20 10:49:19 +0000

    working version

--- src/app/components/home-content/home-content.component.ts
+++ src/app/components/home-content/home-content.component.ts
@@ -303,7 +303,10 @@ export class HomeContentComponent {
   }
 
 
-  
+  removeExercise(int: any){
+    this.currentExercises.splice(int,1);
+    console.log(this.currentExercises);
+  }
   checkOrInsertUser(user: User) {
     if (!this.user.isProfileCreated) {
       this.userService.addUser(user).subscribe({
